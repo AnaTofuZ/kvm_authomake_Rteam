b605605df8cc48cca3ae331542a3deb4b3aa8e22
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-05-23 09:06:40 +0900

    new

---
+++ kvm-add-ip.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Getopt::Long qw(:config auto_help);
+
+# オプション指定
+
+GetOptions(
+    \my %opt,
+    "group=s","num=i",
+);
+
+# whoami でユーザーが取れるのでrootか判断
+
+if ( $> == 0){
+    print "rootでお願いします!\n";
+    die; 
+}
+
+die "groupを指定してください "if (! $opt{group});
+
+my $group =undef;
+
+$group = "flogs" if $opt{group}=~ /^[fF].*/;
+$group = "jelly" if $opt{group}=~ /^[jJ].*/;
+$group = "dande" if $opt{group}=~ /^[dD].*/;
+
+die "有効なgropuを指定して" if !defined $group; 
+
+my $number_vm = 0;
+my $text = "./.group/$group";
+
+if (! $opt{num}) {
+
+    open(my $fh,'<',$text);
+    $number_vm =$_ while (<$fh>);
+
+} else {
+    $number_vm = $opt{num};
+}
+
+system("virsh-clone");
+
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+kvm.pl - VMインスタンスをよしなに作るやつ
+
+=head1 SYNOPSIS
+
+    ./kvm-add-ip.pl --group jelly --num 5
+
+=head1 OPTIONS
+
+=head2 --group=Gropus
+グループ名を指定 (dandelion,flogs,jellyfish)
+
+=head2 --num=NUM
+
+作るVMの番号を指定
+
+=head1 AUTHOR
+
+Takahiro SHIMIZU E<lt>e155730@ie.u-ryukyu.ac.jpE<gt>
+
+=cut
